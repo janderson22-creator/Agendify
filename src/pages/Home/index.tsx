e4f9d6bd7a59b827d32a53d1c81516b432eeef34
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-01-16 23:01:04 -0300

    tailwind added

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles";
+
+const Home: React.FC = () => {
+  return (
+    <S.Container>
+      <div className="bg-sky-500 w-10 h-10 p-10 m-auto flex items-center justify-center rounded mt-10">
+        Hello world!
+      </div>
+    </S.Container>
+  );
+};
+
+export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Route, Switch } from 'react-router-dom'
-import Home from '../pages'
+import Home from '../pages/Home'
 
 export const Routes: React.FC = () => (
     <Switch>
