fbfc084dc00d656699acb3d47fad167e5fdc9db6
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-01-17 23:07:49 -0300

    menu side bar, navigation

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+interface Props {
+  checked: boolean;
+}
+
+export const Container = styled.div`
+  border: transparent;
+  border-radius: 0 50px 50px 0;
+  position: fixed;
+  left: 0;
+  top: 50px;
+  width: 210px;
+  height: 90%;
+  background: linear-gradient(
+      86.57deg,
+      #373740 47.14%,
+      #2f3a59 90.82%,
+      #738cbf 167.05%
+    ),
+    linear-gradient(0deg, #ffffff, #ffffff);
+  box-shadow: 2px 5px 20px 1px rgba(0, 0, 0, 0.4);
+`;
+
+export const ItemsMenu = styled.div`
+  padding: 0 15px;
+  padding-top: 20px;
+`;
+
+export const ContainerLink = styled.div<Props>`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  height: 45px;
+  margin-bottom: 10px;
+  border-radius: 200px;
+  padding-left: 15px;
+  background: ${(props) => (props.checked ? "#2F3A59" : "transparent")};
+
+  :hover {
+    background: #2f3a59;
+    span {
+      color: rgba(244, 244, 246, 1);
+    }
+    div {
+      svg {
+        path {
+          fill: #f4f4f6;
+        }
+      }
+    }
+  }
+`;
+
+export const Icon = styled.div<Props>`
+  svg {
+    path {
+      fill: ${(props) => (props.checked ? "#F4F4F6" : "#BBBBD2")};
+    }
+  }
+`;
+
+export const NameLink = styled.span<Props>`
+  /* display: ${(props: any) => (props.collapsedd ? "none" : "block")}; */
+  font-size: 14px;
+  font-weight: 600;
+  color: ${(props: any) =>
+    props.checked ? "rgba(244, 244, 246, 1)" : "rgba(202, 202, 229, 1)"};
+  margin-left: 15px;
+`;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,4 +9,15 @@ export default createGlobalStyle`
     outline: 0 !important;
     font-family: 'Roboto', sans-serif;
   }
+
+  body {
+    max-width: 1280px;
+    margin: 0 auto;
+  }
 `
+
+// cor1: #373740 grey dark
+// cor2: #2F3A59 blue secondary
+// cor3: #738CBF blue main
+// cor4: #8593A6 grey 
+// cor5: #0D0D0D black
