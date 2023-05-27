bfbf9232fd60934ab57d6d6e01932088a57c3938
janderson22-creator <jandersomcosta@yahoo.com.br>
2023-05-27 10:50:37 -0300

    section with list of our services

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,18 +1,11 @@
 import * as S from "./styles";
-import Calendar from "react-calendar";
 import { useState } from "react";
 
 const CalendarCommerce: React.FC = () => {
   const [value, onChange] = useState(new Date());
   return (
     <S.Container>
-      <Calendar
-        defaultActiveStartDate={value}
-        calendarType="US"
-        onClickDay={(e) => console.log(e)}
-        onChange={onChange}
-        value={value}
-      />
+      
     </S.Container>
   );
 };
